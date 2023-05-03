77e341daa5670ca92700d0a3ee7b363f120d7737
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-03 22:55:41 +0900

    Add: 分割代入と残余引数を復習

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -1,22 +1,22 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
 
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+const result = Math.max(...nums);
 console.log(result);
 
 
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
-let newArr = [...arr1];
-
-console.log(newArr2);
+let newArr = [...arr1, 10, ...arr2];
+console.log(newArr);
 
 const obj = {
   name: "Tom",
   age: 22,
 };
 const newObj = { ...obj };
+newObj.name = "Bob";
 
-console.log(newObj);
+console.log(newObj, obj);
 
 const restA = (...argA) => console.log(argA);
 restA(1, 3, 4)
