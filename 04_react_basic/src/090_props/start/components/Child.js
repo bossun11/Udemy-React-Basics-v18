6800a7e3375b82e6c481b3bdcdb5e0f852fbaeeb
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-04 22:42:48 +0900

    Add: propsの基本的な使い方を学習

--- 04_react_basic/src/090_props/start/components/Child.js
+++ 04_react_basic/src/090_props/start/components/Child.js
@@ -1,9 +1,13 @@
 import "./Child.css";
 
-const Child = ({ color }) => {
+const Child = ({ color = 'green', num, fn, bool, obj }) => {
   return (
-    <div className={`component ${color}`}>
+    <div className={ `component ${color}` }>
       <h3>Hello Component</h3>
+      <h3>{ num }</h3>
+      <h3>{ fn('Props') }</h3>
+      <h3>{ bool ? 'true' : 'false' }</h3>
+      <h3>{ obj.name + obj.age }</h3>
     </div>
   );
 };
