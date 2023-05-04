7724f8ca1967016823639a27c1890a1948d2ec95
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-04 23:21:36 +0900

    Add: propsのchildrenについて学習

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,29 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
+
+// mapメソッドを使って、配列の要素をコンポーネントに変換する
+const eachProfile = profile.map((profile, index) => {
+  return (
+    <Profile
+      key={index}
+      name={profile.name}
+      age={profile.age}
+      country={profile.country}
+      color={profile.color}
+    />
+  );
+});
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="Childrenとは？">
+        {eachProfile}
+      </Container>
     </div>
   );
 };
