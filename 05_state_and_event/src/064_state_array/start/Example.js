91075f343ac9c87a732ff7874c59fc3b3d00233f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-05 17:01:39 +0900

    Add: オブジェクトを扱うstateの練習問題を実施

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  const shuffle = () => {
+    const lastVal = nums.pop();
+    nums.unshift(lastVal);
+    setNums([...nums]
+  )};
 
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
