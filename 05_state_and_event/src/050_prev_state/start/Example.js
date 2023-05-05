ac7d293cb21171db9b4339d399de4ddfacbdd8ef
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-05 13:58:46 +0900

    Add: state使用時の注意点について学習

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,27 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  const [count, setCount] = useState(0);
+
+  // カウントアップの関数
+  const countUp = () => {
+    setCount(count + 1);
+    setCount(prevstate => prevstate + 1);
+  };
+
+  // カウントダウンの関数
+  const countDown = () => {
+    if(count === 0) return;
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <p>現在のカウント数: {count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </>
+  )
 };
 
 export default Example;
