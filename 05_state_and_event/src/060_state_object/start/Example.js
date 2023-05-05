f5a279fb47cc999304911ed916ed74462dcb6c9c
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-05 14:28:52 +0900

    Add: stateでオブジェクトを扱う際の注意点について学習

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -2,6 +2,31 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj);
+  const { name, age } = person;
+
+  const changeName = (e) => {
+    setPerson({ ...person, name: e.target.value });
+  };
+
+  const changeAge = (e) => {
+    setPerson({ ...person, age: e.target.value });
+  };
+
+  const resetButton = () => {
+    setPerson({ name: "", age: "" });
+  };
+
+  return (
+    <>
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <input type="text" value={name} onChange={changeName} />
+      <input type="number" value={age} onChange={changeAge} />
+      <br />
+      <button onClick={resetButton}>リセット</button>
+    </>
+  )
 };
 
 export default Example;
