fd58eb87fdd6cd150722eae3d00f2b56e0a20f80
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-06 15:54:23 +0900

    Add: チェックボックスの作成方法を学習

--- 06_control_and_form/src/100_single_checkbox/start/Example.js
+++ 06_control_and_form/src/100_single_checkbox/start/Example.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [checked, setChecked] = useState(false);
+
+  const toggleChecked = () => setChecked(!checked);
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <p style={{ textAlign: "center" }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+      <label>
+        チェック : 
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={toggleChecked}
+        />
+        <div>{checked ? "ON!" : "OFF"}</div>
+      </label>
+    </div>
   );
 };
 
