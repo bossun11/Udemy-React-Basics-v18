7ae140f9e5393312aa2388634543b7528eb1d17f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-06 16:40:42 +0900

    Add: 複数選択チェックボックスの作成方法を学習

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -9,9 +9,52 @@ const Example = () => {
 
   const [sum, setSum] = useState(0);
 
+  const handleChange = (e) => {
+    // 配列のcheckedを変更する
+    const newFluits = fruits.map((fruit) => {
+      const newFruit = { ...fruit };
+      if (newFruit.label === e.target.value) {
+        newFruit.checked = !newFruit.checked;
+      }
+      return newFruit;
+    });
+    setFruits(newFluits);
+
+    // let sumVal = 0;
+    // newFluits.forEach((fruit) => {
+    //   if (fruit.checked) {
+    //     sumVal += fruit.value;
+    //   }
+    // });
+
+    // newFluits
+    //   .filter((fruit) => fruit.checked)
+    //   .forEach((fruit) => {
+    //   sumVal += fruit.value;
+    // });
+
+    const sumVal = newFluits
+      .filter((fruit) => fruit.checked)
+      .reduce((sum, fruit) => sum + fruit.value, 0);
+    setSum(sumVal);
+  }
+
   return (
     <div>
-      
+      {fruits.map((fruit) => {
+        return (
+          <div key={fruit.label}>
+            <input
+              type="checkbox"
+              value={fruit.label}
+              onChange={handleChange}
+              id={fruit.label}
+              checked={fruit.checked}
+            />
+            <label htmlFor={fruit.label}>{fruit.label}:{fruit.value}</label>
+          </div>
+        )
+      })}
       <div>合計：{sum}</div>
     </div>
   );
