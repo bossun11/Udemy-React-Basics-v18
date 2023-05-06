450a347f0d48dd394f1753dd9006a3f202a2928f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-06 16:49:59 +0900

    Add: プルダウンの作成方法を学習

--- 06_control_and_form/src/120_select/start/Example.js
+++ 06_control_and_form/src/120_select/start/Example.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [selected, setSelected] = useState("Apple");
+  const OPTIONS = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <p style={{ textAlign: "center" }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        {OPTIONS.map((option) => <option key={option} value={option}>{option}</option>)}
+      </select>
+    </>
   );
 };
 
