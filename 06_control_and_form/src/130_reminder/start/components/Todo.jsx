b4f9565e3d2c023910edf0dd417822b103f52cf5
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-06 22:50:52 +0900

    Add: Todoアプリの作成方法を学習
    自力でTodo自体は作成出来たが、HTMLで理解してない箇所がちょいちょいあるので、formタグにより処理の挙動がうまくいかなかった。

---
+++ 06_control_and_form/src/130_reminder/start/components/Todo.jsx
@@ -0,0 +1,42 @@
+import { List } from "./List";
+import { Form } from "./Form";
+import { useState } from "react";
+
+
+export const Todo = () => {
+  const todosList = [
+    {
+      id: 1,
+      content: "店予約する",
+    },
+    {
+      id: 2,
+      content: "卵買う",
+    },
+    {
+      id: 3,
+      content: "郵便出す",
+    },
+  ];
+
+  const [todos, setTodos] = useState(todosList);
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(newTodos);
+  }
+
+  const createTodo = (todo) => {
+    setTodos([...todos, todo]);
+  }
+
+  return (
+    <>
+      <h2>Reminder</h2>
+      <List todos={todos} deleteTodo={deleteTodo} />
+      <Form createTodo={createTodo} />
+    </>
+  )
+};
