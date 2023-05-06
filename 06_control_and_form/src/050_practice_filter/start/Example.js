247e5d64c46f15470a5748aa0a3994adedaf1a4f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-06 10:33:55 +0900

    Add: フィルターメソッドの練習問題を実施

--- 06_control_and_form/src/050_practice_filter/start/Example.js
+++ 06_control_and_form/src/050_practice_filter/start/Example.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Profile from "./components/Profile";
 
 const persons = [
@@ -19,12 +20,17 @@ const persons = [
 ];
 
 const Example = () => {
+  const [inputVal, setInputVal] = useState("")
+
   return (
     <>
       <h3>練習問題</h3>
       <p>入力欄を設置して、入力値と名前が一致したもののみ表示する仕組みを作成してください。</p>
+      <input type="text" value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
       <ul>
-        {persons.map((person) => (
+        {persons
+        .filter(person => person.name.includes(inputVal))
+        .map((person) => (
           <li key={person.name}>
             <Profile {...person} />
           </li>
