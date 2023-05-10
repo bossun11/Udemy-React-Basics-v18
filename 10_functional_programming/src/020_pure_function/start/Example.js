e4eed44e8ccc2e10008c75f889f667f6df2d6692
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-05-10 10:35:49 +0900

    Add: 純粋関数について学習

--- 10_functional_programming/src/020_pure_function/start/Example.js
+++ 10_functional_programming/src/020_pure_function/start/Example.js
@@ -1,8 +1,18 @@
 const Example = () => {
+
+  const val1 = 3, val2 = 9;
+
+  let result;
+
+  const add = (val1) => {
+    result = val1 + val2;
+    return val1 + val2;
+  }
   return (
     <>
       <h3>純粋関数</h3>
       <p>fn(決まった引数) には 決まった戻り値 を返す</p>
+      <div>純粋関数：{add(val1)}</div>
     </>
   );
 };
