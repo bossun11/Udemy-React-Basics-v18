be5b76360baa76971b3d7abbd5e598f8ff345507
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-02 17:26:02 +0900

    Fix: endフォルダのバグを修正

--- 13_redux/end/src/010_redux_no_rtk/components/CounterResult.js
+++ 13_redux/end/src/010_redux_no_rtk/components/CounterResult.js
@@ -1,7 +1,7 @@
-import { useCounter } from "../context/CounterContext";
-import { useSelector } from "react-redux"
+// import { useCounter } from "../context/CounterContext";
+import { useSelector } from "react-redux";
 const CounterResult = () => {
-  const state = useSelector(state => state);
+  const state = useSelector((state) => state);
   return <h3>{state}</h3>;
 };
 
