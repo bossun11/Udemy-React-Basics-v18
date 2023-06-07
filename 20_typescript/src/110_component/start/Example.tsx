c8025847dce495d4638ee73e46b0feea71b509b0
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 22:47:55 +0900

    Add: 関数コンポーネントへの型定義の記述を学習

--- 20_typescript/src/110_component/start/Example.tsx
+++ 20_typescript/src/110_component/start/Example.tsx
@@ -1,5 +1,8 @@
-const Example = () => {
-    // 練習
+import Hello from "./Hello";
+
+// React.VFCはReactバージョン18から非推奨になった
+const Example: React.FC = () => {
+  return <Hello />;
 };
 
 export default Example;
