da95eb1eb9a6f26887d8abc4acd710b4bccf54e0
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 17:40:30 +0900

    Add: TypeScriptの型推論について学習
    外部からデータを持ってくる場合や他者が使うようなデータにおいては基本的に型定義をすること。

--- 20_typescript/src/060_type_inference/start/Example.tsx
+++ 20_typescript/src/060_type_inference/start/Example.tsx
@@ -1,5 +1,12 @@
 const Example = () => {
   // 練習用
+  // TypeScriptの型推論
+  let str = "Hello";
+  let obj = { name: "Jack", age: 21 };
+
+  // リテラル型の型推論
+  const bye = "Bye";
+  const num = 1;
 };
 
 export default Example;
