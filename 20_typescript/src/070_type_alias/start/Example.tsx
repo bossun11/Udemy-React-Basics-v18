11bc9e6afd36f7f8cc6a7d38ac448c19551fb93e
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 20:26:43 +0900

    Add: 型エイリアスについて学習
    定義した型を変数みたいに使えるようになる

--- 20_typescript/src/070_type_alias/start/Example.tsx
+++ 20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,30 @@
 const Example = () => {
   // 練習用
+  type User = {
+    name: string;
+    age: number;
+  };
+
+  const user: User = {
+    name: "Taro",
+    age: 12,
+  };
+
+  type UserName = string;
+  type UserAge = number;
+  type UserGender = "man" | "woman" | "others";
+
+  type UserProfile = {
+    name: UserName;
+    age: UserAge;
+    gender: UserGender;
+  };
+
+  const userProfile: UserProfile = {
+    name: "イレイナ",
+    age: 18,
+    gender: "woman",
+  };
 };
 
 export default Example;
