c5838fb0ca05fde73268657f3ad27e9f2c4190ea
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 22:38:45 +0900

    Add: ジェネリクスについて学習
    Add: 型を引数として取れる記述のこと。関数実行時に型を引数として定義する必要がある。型の複数指定も可能。

--- 20_typescript/src/090_generics/start/Example.ts
+++ 20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,24 @@
 const Example = () => {
   // 練習用
+  const repeatStr = (value: string, times: number): string[] => {
+    return new Array(times).fill(value);
+  };
+  const repeatNum = (value: number, times: number): number[] => {
+    return new Array(times).fill(value);
+  };
+
+  // ジェネリクス
+  const repeat = <T>(value: T, times: number): T[] => {
+    return new Array(times).fill(value);
+  };
+
+  const strArray = repeat<string>("Hello", 3);
+  const numArray = repeat<number>(10, 3);
+  console.log(strArray);
+
+  // const strArray = repeatStr("Hello", 3);
+  // const numArray = repeatNum(10, 3);
+  // console.log(strArray);
 };
 
 export default Example;
