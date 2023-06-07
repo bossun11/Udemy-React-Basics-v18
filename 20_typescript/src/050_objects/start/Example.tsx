11b65b6bc4a58bfabf23d200b8286c4736706373
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 17:29:33 +0900

    Add: オブジェクトと配列の型の定義方法について学習

--- 20_typescript/src/050_objects/start/Example.tsx
+++ 20_typescript/src/050_objects/start/Example.tsx
@@ -1,5 +1,23 @@
 const Example = () => {
   // 練習用
+  // 配列の型定義
+  const arr1: number[] = [0, 1, 2];
+  const arr2: string[] = ["Hello", "Bye"];
+  const arr3: Array<number> = [0, 1, 2];
+  const arr4: (string | number)[] = ["Hello", 1, 2];
+  const arr5: Array<string | number> = ["Hello", 1, 2];
+
+  // オブジェクトの型定義
+  type Person = { name: string; age?: number };
+  const obj1: Person = { name: "Jack", age: 21 };
+  // const obj1: { name: string; age: number } = { name: "Jack", age: 21 };
+
+  // 配列の中にオブジェクトが入っている場合の型定義
+  const users: Person[] = [
+    { name: "Taro", age: 18 },
+    { name: "Jon" },
+    { name: "Mike", age: 15 },
+  ];
 };
 
 export default Example;
