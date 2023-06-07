8d056010f01676a05d5dc46c362e3a5d9c1a5254
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 17:12:35 +0900

    Add: ユニオン型の定義方法について学習

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,18 @@
 const Example = () => {
   // 練習用
+  let strOrNum: string | number = "Hello";
+  strOrNum = 1000;
+  console.log(strOrNum);
+
+  let strOrNumOrBool: string | number | boolean = "Hello";
+  strOrNumOrBool = false;
+  console.log(strOrNumOrBool);
+
+  type HelloOrNum = string | number;
+  const hello: HelloOrNum = "Hello";
+
+  type DayOfWeek = "Monday" | "Tuesday";
+  const day: DayOfWeek = "Monday";
 };
 
 export default Example;
