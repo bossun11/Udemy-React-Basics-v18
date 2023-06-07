f2dfece3723e34134b10ca9329999ff860363dea
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-07 16:49:56 +0900

    Add: プリミティブ型の型定義について学習

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,30 @@
 const Example = () => {
   // 練習用
+  let str: string = "Hello";
+  str = "World";
+  console.log(str);
+
+  let num: number = 10;
+  console.log(num);
+
+  let bigNum: bigint = 1000n;
+  console.log(bigNum);
+
+  let bool: boolean = false;
+  console.log(bool);
+
+  let nullish: null = null;
+  let undefinedValue: undefined = undefined;
+
+  // リテラル型
+  let trueVal: true = true;
+  let num123: 123 = 123;
+  let hello: "Hello" = "Hello";
+
+  //以下はエラーになる
+  // let trueVal: true = false;
+  // let num123: 123 = 456;
+  // let hello: "Hello" = "World";
 };
 
 export default Example;
