695d11a9ea2e7439134c65555c9de027b77666e6
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-08 10:19:53 +0900

    Add: useStateの型定義の記述について学習

--- 20_typescript/src/150_usestate/start/Example.tsx
+++ 20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 
+type User = {
+  name: string;
+  age: number;
+};
+
+type Todo = {
+  id: number;
+  text: string;
+};
+
 const Example = () => {
-  
+  const [text, setText] = useState("initial value");
+  useState(0);
+
+  const [animals, setAnimals] = useState<string[]>(["dog", "cat"]);
+  const [user, setUser] = useState<User[]>([{ name: "John", age: 20 }]);
+  const [todo, setTodo] = useState<Todo[]>([{ id: 1, text: "hello" }]);
 };
 
 export default Example;
