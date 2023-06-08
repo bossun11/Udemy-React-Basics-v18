3f748482768f6994145dfcb726c3c5e19d587b85
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-08 11:02:25 +0900

    Add: TypeScriptを使って簡単なTODOリストを作成

--- 20_typescript/src/160_add_list_item/start/Example.tsx
+++ 20_typescript/src/160_add_list_item/start/Example.tsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
+import TaskList from "./components/TaskList";
+
+export type Todo = {
+  id: number;
+  text: string;
+};
+
+const initTodoList = [
+  { id: 1, text: "Todo" },
+  { id: 2, text: "Todo2" },
+];
 
 const Example = () => {
-  
+  const [todos, setTodos] = useState<Todo[]>(initTodoList);
+  const [inputText, setInputText] = useState<string>("");
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const addTodoItem = () => {
+    setTodos((state) => [...state, { id: state.length + 1, text: inputText }]);
+    setInputText("");
+  };
+
+  return (
+    <>
+      <input value={inputText} onChange={changeHandler} />
+      <button onClick={addTodoItem}>追加</button>
+      <TaskList todos={todos} />
+    </>
+  );
 };
 
 export default Example;
